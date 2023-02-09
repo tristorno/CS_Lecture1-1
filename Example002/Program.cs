709b7ebddc8a4478f7f9fbf586a68f18acfa75f6
tristorno <tristorn@mail.ru>
2023-02-09 13:33:31 +0300

    Привет Надюхе!

---
+++ Example002/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("Как тебя зовут?");
+string username = Console.ReadLine ();
+Console.Write ("Нарушаешь мои границы, ");
+Console.Write (username);
+Console.WriteLine (". БЕРЕГИСЬ!");
