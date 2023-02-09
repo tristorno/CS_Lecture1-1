fb14f326ced47f6660f465efc6911aa739eaccb9
tristorno <tristorn@mail.ru>
2023-02-09 18:55:00 +0300

    гири 1

---
+++ Example005_giri/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 3;
+int c = 9;
+int d = 6;
+int e = 5;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
